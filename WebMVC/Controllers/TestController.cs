ddf9c2a4a96d0a1a9569fe916e0aeccab9a7fb4b
Chen Ming <182802469@163.com   name=chen ming>
2017-12-24 15:56:45 +0800

    first commit

---
+++ WebMVC/Controllers/TestController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string GetString()
+        {
+            return "Hello World is old now. It's time for wassup bro ;)";
+        }
+
+        public ActionResult GetView()
+        {
+            Employee emp = new Employee
+            {
+                FirstName = "Sukesh",
+                LasrName = "Marla",
+                Salary = 20000,
+            };
+            //ViewData["Employee"] = emp;
+            //ViewBag.Employee = emp;
+            //
+            return View("ModelView",emp);
+        }
+
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustorerName = "Customer1";
+            c.Address = "Address1";
+            return c;
+        }
+
+        [NonAction]
+        public string SimpleAction()
+        {
+            return "I am not a simple action";
+        }
+    }
+
+    public class Customer
+    {
+        public string CustorerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.CustorerName + "|" + this.Address;
+        }
+    }
+}
---
+++ WebMVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LasrName { get; set; }
+        public int Salary { get; set; }
+    }
+}
